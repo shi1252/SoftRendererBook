f6fc1e6842c0153a9321859766e6d1533aeae336
이규한 <a>
2020-05-20 19:16:59 +0900

    mod

--- Source/Runtime/Engine/Private/Transform.cpp
+++ Source/Runtime/Engine/Private/Transform.cpp
@@ -3,12 +3,12 @@
 
 Matrix3x3 Transform::GetModelingMatrix() const
 {
-	Vector3 tCol(Position._X, Position._Y, 1);
-	Matrix3x3 translationMat(Matrix3x3::Identity._Cols[0], Matrix3x3::Identity._Cols[1], tCol);
+	Vector3 tCol(Position.X, Position.Y, 1);
+	Matrix3x3 translationMat(Matrix3x3::Identity.Cols[0], Matrix3x3::Identity.Cols[1], tCol);
 	float rotation = Math::Deg2Rad(Rotation);
 	Vector3 rXAxis(cosf(rotation), sinf(rotation), 0), rYAxis(-sinf(rotation), cosf(rotation), 0);
-	Matrix3x3 rotationMat(rXAxis, rYAxis, Matrix3x3::Identity._Cols[2]);
-	Matrix3x3 scaleMat(Matrix3x3::Identity._Cols[0] * Scale._X, Matrix3x3::Identity._Cols[0] * Scale._Y, Matrix3x3::Identity._Cols[2]);
+	Matrix3x3 rotationMat(rXAxis, rYAxis, Matrix3x3::Identity.Cols[2]);
+	Matrix3x3 scaleMat(Matrix3x3::Identity.Cols[0] * Scale.X, Matrix3x3::Identity.Cols[0] * Scale.Y, Matrix3x3::Identity.Cols[2]);
 	return translationMat * rotationMat * scaleMat;
 }
 
